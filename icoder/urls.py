687a37fa7763b4fcd5057e926b36f6a1587d5573
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2022-03-14 16:24:53 +0530

    update

--- icoder/urls.py
+++ icoder/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 
-from django.views.static import serve
-from django.conf.urls import url
+# from django.views.static import serve
+# from django.conf.urls import url
 
 admin.site.site_header="MyBlog Admin"
 admin.site.site_title="MyBlog Admin Panel"
@@ -27,6 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('blog/', include('blog.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
